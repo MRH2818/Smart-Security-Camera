bf784bfdef0d10f8bcf8b92fa1301da885dda84d
Max Huddleston <70760644+MRH2818@users.noreply.github.com>
2021-08-05 13:53:28 -0400

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,13 +1,25 @@
 import smtplib
+import json
+import os
 from email.message import EmailMessage
 
+def load_config_file () :
+    file_text = ""
+    with open(os.path.join("Data", "config.json"), "r") as file_data :    
+        file_text = "".join(file_data.readlines())
+    
+    return json.loads(file_text)
+
 def send_email_message (file_path, person_name, image_type="jpeg", image_file_name="Surveilance Photo.jpeg") :
+    # Load data from config file
+    config = load_config_file()
+    
     # Format message
     message = EmailMessage()
 
     message["Subject"] = "Alert From Your Security Camera"
-    message["From"] = "TerribleUsername01@gmail.com"
-    message["To"] = "TerribleUsername00@yahoo.com"
+    message["From"] = config["email_sender"]
+    message["To"] = config["email_recipient"]
 
     message.set_content(f'Dear Max,\n\n{person_name} has entered your room. Please do not worry, this incident has been logged.\n\nUntil next time,\nMax\'s Security Camera')
 
@@ -18,22 +30,22 @@ def send_email_message (file_path, person_name, image_type="jpeg", image_file_na
     message.add_attachment(file_data, maintype="image", subtype=image_type, filename=image_file_name)
 
     # Establish connection and send message
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtpConn :
-        smtpConn.login("TerribleUsername01@gmail.com", "AtrociousPassword")
-
-        # Send message
+    with smtplib.SMTP_SSL(config["email_network_smtp_address"], config["email_network_port"]) as smtpConn :
+        smtpConn.login(config["email_sender"], config["email_sender_pass"])
         smtpConn.send_message(message)
 
-# For the Specifically for Ngrok Tunnel URL
 def send_url_message (url) :
+    # Load data from config file
+    config = load_config_file()
+
     message = EmailMessage()
     message["Subject"] = "Your Ngrok Tunnel URL"
-    message["From"] = "TerribleUsername01@gmail.com"
-    message["To"] = "TerribleUsername00@gmail.com"
+    message["From"] = config["email_sender"]
+    message["To"] = config["email_recipient"]
 
     message.set_content(f'Dear Max,\n\nThe Ngrok tunnel URL for your security camera is {url}\n\n'
         + "Until next time,\nMax's Security Camera")
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_conn :
-        smtp_conn.login("TerribleUsername01@gmail.com", "AtrociousPassword")
-        smtp_conn.send_message(message)
+    with smtplib.SMTP_SSL(config["email_network_smtp_address"], config["email_network_port"]) as smtpConn :
+        smtpConn.login(config["email_sender"], config["email_sender_pass"])
+        smtpConn.send_message(message)
